0928b4961ce31f55513a6885c3b4a94a7b53a2d4
Vlad <vladislav.sytyuk@gmail.com>
2023-08-17 17:53:16 +0300

    Add Dockerfile

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import sys
 import os
 
 from fastapi import FastAPI
@@ -6,6 +7,13 @@ from app.config import settings
 from app.api.api_v1.api import api_router
 
 app = FastAPI()
+
+try:
+    __import__("pysqlite3")
+    sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+except ModuleNotFoundError:
+    pass
+
 
 os.environ["OPENAI_API_KEY"] = settings.OPENAI_API_KEY
 
